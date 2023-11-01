7b1bb3f080434e560c8173b363e12ac18b01596b
mtnleo <mtnleonardi@gmail.com>
2023-11-01 16:53:44 -0300

    API ya funcionando en el HTML (muy rudimentario todo, pero es la base)

--- CorteQuiz/src/app/pregunta-respuesta/pregunta-respuesta.component.ts
+++ CorteQuiz/src/app/pregunta-respuesta/pregunta-respuesta.component.ts
@@ -1,31 +1,39 @@
-/*import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pregunta-respuesta',
   templateUrl: './pregunta-respuesta.component.html',
   styleUrls: ['./pregunta-respuesta.component.css']
 })
-export class PreguntaRespuestaComponent {
 
+export class PreguntaRespuestaComponent {
+	url_api: string = "https://the-trivia-api.com/v2/questions/";
+
+	constructor() {}
+
+	pregunta: string = "";
+	respuestaCorrecta: string = "";
+	respuestasIncorrectas: string[] = [];
+
+	async getPreguntaRespuesta() {
+		try {
+			const response = await fetch(this.url_api);
+			if (response.ok) {
+				const data = await response.json();
+
+				this.pregunta = data[0].question["text"];
+				this.respuestaCorrecta = data[0].correctAnswer;
+				this.respuestasIncorrectas = data[0].incorrectAnswers;
+			}
+			else {
+				throw new Error("Error status code: " + response.status);
+			}
+		} catch (error) {
+			console.error("Error! ", error);
+		}
+	}
+	
 }
-*/
 
 // LA API FUNCIONA PERFECTAMENTE
-let url_api = "https://the-trivia-api.com/v2/questions/";
 
-async function getPreguntaRespuesta() {
-    try {
-		const response = await fetch(url_api);
-		if (response.ok) {
-			const data = await response.json();
-			console.log(data);
-		}
-		else {
-			throw new Error("Codigo de error: " + response.status);
-		}
-	} catch (error) {
-		console.error("Error! ", error);
-	}
-}
-
-getPreguntaRespuesta();
