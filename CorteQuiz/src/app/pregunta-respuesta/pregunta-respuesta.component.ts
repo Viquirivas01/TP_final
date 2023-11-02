d9b485d311d17a5e4ab4c7ad31e5850a1e7279d2
mtnleo <mtnleonardi@gmail.com>
2023-11-02 11:25:56 -0300

    Preguntas y respuestas ahora funciona con botones y te tira por alert la respuesta correcta

--- CorteQuiz/src/app/pregunta-respuesta/pregunta-respuesta.component.ts
+++ CorteQuiz/src/app/pregunta-respuesta/pregunta-respuesta.component.ts
@@ -13,7 +13,8 @@ export class PreguntaRespuestaComponent {
 
 	pregunta: string = "";
 	respuestaCorrecta: string = "";
-	respuestasIncorrectas: string[] = [];
+	respuestasDesordenadas: string[] = [];
+	posRespuestaCorrecta: number = -1;
 
 	async getPreguntaRespuesta() {
 		try {
@@ -23,7 +24,7 @@ export class PreguntaRespuestaComponent {
 
 				this.pregunta = data[0].question["text"];
 				this.respuestaCorrecta = data[0].correctAnswer;
-				this.respuestasIncorrectas = data[0].incorrectAnswers;
+				this.desordenarRespuestas(data[0].incorrectAnswers);
 			}
 			else {
 				throw new Error("Error status code: " + response.status);
@@ -31,6 +32,28 @@ export class PreguntaRespuestaComponent {
 		} catch (error) {
 			console.error("Error! ", error);
 		}
+	}
+
+	desordenarRespuestas(respuestasIncorrectas: string[]) {
+		this.posRespuestaCorrecta = Math.floor(Math.random() * 4);
+		this.respuestasDesordenadas[this.posRespuestaCorrecta] = this.respuestaCorrecta;
+		let j: number = 0;
+		for (let i = 0; i < 4; i++) {
+			if (i !== this.posRespuestaCorrecta) {
+				this.respuestasDesordenadas[i] = respuestasIncorrectas[j];
+				j++;
+			}
+		}
+	}
+
+	verificarRespuesta(posRespuestaElegida: number) {
+		if (this.posRespuestaCorrecta === posRespuestaElegida) {
+			alert("Correcta!");
+		}
+		else {
+			alert("Respuesta incorrecta: Respuesta es " + this.respuestaCorrecta);
+		}
+		this.getPreguntaRespuesta();
 	}
 	
 }
