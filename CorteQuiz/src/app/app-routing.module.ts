4cbbb98d0855ee9e4ba32bda69ad8152eb8712d6
Viquirivas01 <victoriarivas01@outlook.es>
2023-11-22 22:17:14 -0300

    guard
    no funciona correctamente

--- CorteQuiz/src/app/app-routing.module.ts
+++ CorteQuiz/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MultijugadorComponent } from './components/multijugador/multijugador.component';
+import { noCreadoGuard } from './no-creado.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'MultijugadorComponent', component: MultijugadorComponent, canActivate: [noCreadoGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CorteQuiz/src/app/components/multijugador/multijugador.component.ts
+++ CorteQuiz/src/app/components/multijugador/multijugador.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { noCreadoGuard } from 'src/app/no-creado.guard';
 
 @Component({
   selector: 'app-multijugador',
   templateUrl: './multijugador.component.html',
   styleUrls: ['./multijugador.component.css']
 })
-export class MultijugadorComponent {
 
+export class MultijugadorComponent {
 }
